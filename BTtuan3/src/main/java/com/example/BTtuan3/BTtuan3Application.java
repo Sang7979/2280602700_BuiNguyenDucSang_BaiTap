a35d2012d5d60f2eb220ac397952f3de5a3ba495
Sang7979 <sang9102004@gmail.com>
2026-01-31 16:04:51 +0700

    Add files via upload

---
+++ BTtuan3/src/main/java/com/example/BTtuan3/BTtuan3Application.java
@@ -0,0 +1,13 @@
+package com.example.BTtuan3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BTtuan3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BTtuan3Application.class, args);
+	}
+
+}
---
+++ BTtuan3/src/test/java/com/example/BTtuan3/BTtuan3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BTtuan3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BTtuan3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
